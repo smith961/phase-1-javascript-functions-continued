ecb98d115e3195173f488a594c354f8e0ab7a157
unknown <smithbabawande@gmail.com>
2023-08-08 15:41:53 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun (activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(activity="go to the office."){
+    return `This Monday, I will ${activity}`;
+    
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home."));
+
+function wrapAdjective(result = "*"){
+return function(emphatic="a hard worker"){
+return`You are ${result}${emphatic}${result}!`;
+}
+}
+console.log(wrapAdjective());
+console.log(wrapAdjective("||"));
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -23,7 +23,7 @@ describe("index.js", () => {
     })
 
     it("permits the default activity to be overriden", function() {
-      expect(mondayWork("work from home")).to.equal("This Monday, I will work from home.")
+      expect(mondayWork("work from home.")).to.equal("This Monday, I will work from home.")
     })
   })
 
